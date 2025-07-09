4a4d38d01ba3a7e74db38cb01658b912088f8437
Dappermarce <Marcecarmek@icloud.com>
2025-07-08 22:23:16 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,13 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
-import InteractiveBrainMap from "@/components/interactive-brain-map"; // Usa esta si el archivo está en /components
-// o: import InteractiveBrainMap from "@/pages/interactive-brain-maps"; // si está en /pages
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={InteractiveBrainMap} />
-      <Route path="/2d" component={InteractiveBrainMap} />
+      <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
